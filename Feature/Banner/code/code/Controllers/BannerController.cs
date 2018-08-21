714dd662e0cc1fbd021e970011d1f65033fcafd9
shahbaaz12 <39556985+shahbaaz12@users.noreply.github.com>
2018-08-21 13:59:18 +0530

    Added a Layout and a basic banner (also a blog section WIP)

---
+++ Feature/Banner/code/code/Controllers/BannerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScHelix.Feature.Banner.Controllers
+{
+    public class BannerController : Controller
+    {
+        // GET: Banner
+        public ActionResult BasicBanner()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Feature/Blog/code/Controllers/ArticleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScHelix.Feature.Blog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public ArticleController()
+        {
+
+        }
+        public ActionResult List()
+        {
+
+            return View();
+        }
+
+        public ActionResult Display()
+        {
+
+            return View();
+        }
+    }
+}
